7eddd17173c476f3053838c239448c7ca5126463
Gustavo Sampaio <sampaioglima@gmail.com>
2021-08-08 01:22:17 -0300

    Handle failed calls and upgrade to ES6.

--- js/index.js
+++ js/index.js
@@ -3,98 +3,96 @@
 */
 
 $(document).ready( () => {
+    'use strict';
 
-    "use strict";
+    let resultList = jQuery('#resultList');
 
-    
-    $("#githubSearchForm").on("submit", function () {
-        
-        var githubSearch = "https://api.github.com/search/repositories?q=";
-        var searchPhrase = $("#searchPhrase").val();
-        var language = $("#langChoice").val();
+    $('#githubSearchForm').on('submit', () => {
+        let githubSearch = 'https://api.github.com/search/repositories?q=';
+        let searchPhrase = $('#searchPhrase').val();
+        let language = $('#langChoice').val();
 
-        resultList.text("Performing query...");
+        resultList.text('Performing query...');
 
         if (searchPhrase) {
-
             githubSearch += encodeURIComponent(searchPhrase);
 
-            if (language != "All") {
-                githubSearch += "+language:" + encodeURIComponent(language);
-            }
+            if (language.toUpperCase() !== 'ALL')
+                githubSearch += `+language:${encodeURIComponent(language)}`;
 
-            // As of jQuery 3.0, the former ".success()" promisse 
-            // was replaced by ".done()", and ".done()" was replaced by ".always()"
+            // As of jQuery 3.0, the former '.success()' promisse 
+            // was replaced by '.done()', and '.done()' was replaced by '.always()'
             $.get(githubSearch)
-                .done(function (results) {
-                    if (results.items.length == 0) {
-                        resultList.text("There are no results for your search :/");
-                    }
-                    else {
+                .done(results => {
+                    if (results.items.length === 0)
+                        resultList.text('There are no results for your search :/');
+                    else
                         displayResults(results.items);
-                        console.log("Response received");
-                    }
                 })
-                .fail(function (err) {
-                    console.log("Failed to query GitHub");
+                .fail((error) => {
+                    console.log('Failed to query GitHub', error);
                 })
                 .always(function () {
-                    console.log("Query executed");
+                    console.log('Query finished');
             });
-    
         }
-        else {
-            resultList.text("You must provide a term or phrase to search for");
-        }
+        else
+            resultList.text('You must provide a term or phrase to search for');
+        console.log(`URL --> ${githubSearch}`);
 
-        console.log("URL --> " + githubSearch);
         return false;
     });
 
-    $("#githubSearchByUserForm").on("submit", function () {
-       
-        var githubSearch = "https://api.github.com/users/";
-        var user = $("#user").val();
+    $('#githubSearchByUserForm').on('submit', () => {
+        let githubSearch = 'https://api.github.com/users/';
+        let user = $('#user').val();
 
-        resultList.text("Performing query...");
+        resultList.text('Performing query...');
 
         if (user) {
-            githubSearch += user + "/repos?sort=stars";
+            githubSearch += `${user}/repos?sort=stars`;
 
             $.get(githubSearch)
-            .done(function (results) {
-                    displayResultsByUser(results);
+                .done(results => {
+                    if (results.length === 0) {
+                        alert('Forbidden value!');
+                        resultList.text('Search for another user.');
+                    }
+                    else
+                        displayResultsByUser(results);
             })
-            .fail(function (err) {
-                console.log("Failed to query GitHub");
+            .fail(error => {
+                if (error.status === 404)
+                    resultList.text('404 - User not found on GitHub.');
+                console.log('Failed to query GitHub', error);
             })
             .always(function () {
-                console.log("Query executed");
+                console.log('Query finished');
             })
         }
-        else {
-            resultList.text("You must provide a usermane");
-        }
+        else
+            resultList.text('You must provide a usermane');
+        console.log(`URL --> ${githubSearch}`);
+
         return false;
     });
-
-    var resultList = jQuery("#resultList");
 
     function displayResultsByUser(results) {
         resultList.empty();
-        $.each(results, function (i, item) {
-            var newResult = 
-                $("<div class='result bordered-element'>" + 
-                "<div class='title'><a href='"+ item.html_url +"' target='_blank'>" + item.name + "</a></div>" +
-                "<div>Language: " + (item.language ? item.language : "No language specified") + "</div>" +
-                "<div>" + (item.description ? item.description : "No description provided") + "</div>" +
-                "<div/>");
+        $.each(results, (i, item) => {
+            let newResult = 
+                $(`<div class='result bordered-element'>` + 
+                `<div class='title'><a href='${item.html_url}' target='_blank'>${item.name}</a></div>` +
+                `<div>Language: ${(item.language ? item.language : 'No language specified')}</div>` +
+                `<div>${(item.description ? item.description : 'No description provided')}</div>` +
+                '<div/>');
 
             newResult.hover(
-                function () {
-                    $(this).css("background-color", "lightgray");
-            }, function (params) {
-                    $(this).css("background-color", "white");
+                () => {
+                    $(this).css('background-color', 'lightgray');
+                },
+                (params) => {
+                    $(this).css('background-color', 'white');
             });
     
             resultList.append(newResult);
@@ -104,20 +102,20 @@ $(document).ready( () => {
     function displayResults(results) {
         resultList.empty();
         $.each(results, function (i, item) {
-            var newResult = 
-                $("<div class='result bordered-element'>" + 
-                "<div class='title'><a href='"+ item.html_url +"' target='_blank'>" + item.full_name + "</a></div>" +
-                "<div>Language: " + item.language + "</div>" +
-                "<div>Stars: " + item.stargazers_count + "</div>" +
-                "<div>Forks: " + item.forks_count + "</div>" +
-                "<div/>");
+            let newResult = 
+                $(`<div class='result bordered-element'>` + 
+                `<div class='title'><a href='${item.html_url}' target='_blank'>${item.full_name}</a></div>` +
+                `<div>Language: ${item.language ? item.language : 'No language specified'}</div>` +
+                `<div>Stars: ${item.stargazers_count}</div>` +
+                `<div>Forks: ${item.forks_count}</div>` +
+                '<div/>');
 
             newResult.hover(
                 function () {
-                    $(this).css("background-color", "lightgray");
+                    $(this).css('background-color', 'lightgray');
                 },
                 function () {
-                    $(this).css("background-color", "white");
+                    $(this).css('background-color', 'white');
                 }
             );
 
@@ -125,115 +123,110 @@ $(document).ready( () => {
         })
     };
 
-    var toggleButton = $("#toggleButton");
-    toggleButton.on("click", function () {
+    let toggleButton = $('#toggleButton');
+    toggleButton.on('click', () => {
         resultList.toggle(250);
-        if (toggleButton.text() === "Hide results") {
-            toggleButton.text("Show results");
-        }
-        else {
-            toggleButton.text("Hide results");
-        }
+        if (toggleButton.text() === 'Hide results')
+            toggleButton.text('Show results');
+        else
+            toggleButton.text('Hide results');
     });
 
-    var navbarItems = $("header nav li");
+    let navbarItems = $('header nav li');
     $.each(navbarItems, function (i, item) {
         $(this).hover(
-            function () {
-                $(this).css("background-color", "#1eb500");
+            () => {
+                $(this).css('background-color', '#1eb500');
             },
-            function () {
-                $(this).css("background-color", "green");
+            () => {
+                $(this).css('background-color', 'green');
             }
         )
     })
 
-    var formButton = $(".formButton");
+    let formButton = $('.formButton');
     formButton.hover(
-        function () {
-            $(this).css("background-color", "green");
+        () => {
+            $(this).css('background-color', 'green');
         },
-        function () {
-            $(this).css("background-color", "rgb(0, 73, 0)");
+        () => {
+            $(this).css('background-color', 'rgb(0, 73, 0)');
         }
     );
 
 
 
 
-
-
-    
-    // var results = [
+    // let results = [
     //     {
-    //         name: "jQuery",
-    //         language: "JavaScript",
-    //         protocol: "HTTP",
+    //         name: 'jQuery',
+    //         language: 'JavaScript',
+    //         protocol: 'HTTP',
     //         score: 4.8,
     //         owner: {
-    //             login: "gustavo",
-    //             password: "12345"
+    //             login: 'gustavo',
+    //             password: '12345'
     //         },
     //         showLog: function () {
-    //             console.log("showLog output");
+    //             console.log('showLog output');
     //         }
     //     },
     //     {
-    //         name: "jQuery UI",
-    //         language: "JavaScript",
-    //         protocol: "HTTP",
+    //         name: 'jQuery UI',
+    //         language: 'JavaScript',
+    //         protocol: 'HTTP',
     //         score: 3.5,
     //         owner: {
-    //             login: "pedro",
-    //             password: "54321"
+    //             login: 'pedro',
+    //             password: '54321'
     //         },
     //         showLog: function () {
-    //             console.log("showLog output");
+    //             console.log('showLog output');
     //         }
     //     },
     // ];
 
-    /* var message = "Hello, JavaScript!";
+    /* let message = 'Hello, JavaScript!';
     console.log(message);
 
-    var resultsDiv = document.getElementById("results");
-    resultsDiv.innerHTML = "This is coming from JS!" */
+    let resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = 'This is coming from JS!' */
     /*
-    results[0].browser = "Edge Dev";
+    results[0].browser = 'Edge Dev';
     console.log(results.length)
     console.log(results[0].browser);
 
     // Looping and conditionals
-    for (var i = 0; i < results.length; i++) {
+    for (let i = 0; i < results.length; i++) {
         if (results[i].score > 4) continue;
-        console.log(results[i].name + " - " + results[i].score)
+        console.log(results[i].name + ' - ' + results[i].score)
 
         console.log(results[i].owner.login);
     }
 
     // Types
-    var aNumber = 4;
-    var trueFalse = true;
-    var Name = "Goose";
-    var none;
+    let aNumber = 4;
+    let trueFalse = true;
+    let Name = 'Goose';
+    let none;
 
-    console.log("aNumber is " + typeof(aNumber));
-    console.log("trueFalse is " + typeof(trueFalse));
-    console.log("Name is " + typeof(Name));
+    console.log('aNumber is ' + typeof(aNumber));
+    console.log('trueFalse is ' + typeof(trueFalse));
+    console.log('Name is ' + typeof(Name));
 
     if (!none)
-    console.log("none is undefined");
+    console.log('none is undefined');
 
-    if (aNumber === "4")
-    console.log("aNumber is of type number and is equal to 4");
+    if (aNumber === '4')
+    console.log('aNumber is of type number and is equal to 4');
     else
-    console.log("We don't know what aNumber is equal to, but its type is not string for sure");
+    console.log('We don't know what aNumber is equal to, but its type is not string for sure');
 
-    var displayOperationName = function (operationName) {
+    let displayOperationName = function (operationName) {
         console.log(operationName);
     }
 
-    //undeclaredVariable = "This should not be legal";
+    //undeclaredletiable = 'This should not be legal';
 
     // Functions
     function Sum (operationName, callback) {
@@ -241,8 +234,8 @@ $(document).ready( () => {
         callback();
     }
 
-    Sum("Sum called", function () {
-        console.log("callback called --> " + (2 + 2));
+    Sum('Sum called', function () {
+        console.log('callback called --> ' + (2 + 2));
     });
 
     */
